652725dd74403b46ede09f14e0869205f7678857
Maxime <yonnet.maxime@laposte.net>
2022-01-04 15:36:58 +0100

    Ajout valeur Alpha global

--- SMA/src/modele/Environnement.java
+++ SMA/src/modele/Environnement.java
@@ -132,7 +132,7 @@ public class Environnement {
 				//energy taken away for each corresponding G
 				valeurToRemove = unModule.getNumberTrueDetruits() * this.energiePriseButProtegeDELTA;
 				
-				System.out.println("L'environnement donne à " + unModule + " une valeur d'énergie égal à " + (valeurToAdd - valeurToRemove));
+				System.out.println("L'environnement donne à " + unModule + " une valeur d'énergie égal à " + (valeurToAdd - valeurToRemove) + ", Pour obtenir une valeur total, Alpha = " + unModule.getSeuilActivationALPHA());
 				unModule.setSeuilActivationALPHA(unModule.getSeuilActivationALPHA() + valeurToAdd - valeurToRemove); // On met à jours alpha
 			}
 		}
