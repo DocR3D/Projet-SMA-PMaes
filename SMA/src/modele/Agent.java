d53fc31cb39f7cc39e5e0e10cb5f839fff7721ee
Gryuh <38225681+Gryuh@users.noreply.github.com>
2022-01-04 15:11:27 +0100

    pattie maes example

--- SMA/src/modele/Agent.java
+++ SMA/src/modele/Agent.java
@@ -1,6 +1,5 @@
 package modele;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Agent {
@@ -9,67 +8,13 @@ public class Agent {
 	private HashMap<String,Boolean> propositionButs;	  			// G
 	private HashMap<String,Boolean> propositionButTerminees;		// R
 	
-	private ArrayList<Object> possession;
-
 	public Agent() {
 		super();
 		
 		this.propositionVraies = new HashMap<String,Boolean>();
 		this.propositionButs = new HashMap<String,Boolean>();
 		this.propositionButTerminees = new HashMap<String,Boolean>();
-		
-		this.possession = new ArrayList<Object>();
-	}
-	
-	//Modules de compétences
-	
-	public Boolean open(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean close(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean lock(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean unlock(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean take(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean release(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean assemble(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean disassemble(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean moveTo(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean search(Object unObjet) {
-		return true;
-	}
-	
-	public Boolean isNear() {
-		return true;
-	}
-	
-	public Boolean isOwner(Object unObjet) {
-		return this.possession.contains(unObjet);
-	}
+		}
 	
 	public HashMap<String,Boolean> S(){
 		return propositionVraies;
