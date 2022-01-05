f888ab4a6043d9cf8de487bccc2042dc949a59a6
Maxime <yonnet.maxime@laposte.net>
2022-01-05 01:33:29 +0100

    Retrait de module qui ne devrait normalement pas être ?
    Possibilité de retourner au commit d'avant pour avoir quelque chose de fonctionnel

--- SMA/src/vue/Main.java
+++ SMA/src/vue/Main.java
@@ -12,101 +12,103 @@ public class Main {
 		Environnement e = new Environnement(20, 45, 70, 20, 50);
 		
 		//Module without conflicts, adds or deletes
-		Module hand_is_empty = new Module((float) 0,"hand_is_empty");
-		Module sander_somewhere = new Module((float) 0,"sander_somewhere");
-		Module sprayer_somewhere = new Module((float) 0,"sprayer_somewhere");
-		Module operationnal = new Module((float) 0,"operationnal");
-		Module board_somewhere = new Module((float) 0,"board_somewhere");
+		//Module hand_is_empty = new Module((float) 0,"hand_is_empty");
+		//Module sander_somewhere = new Module((float) 0,"sander_somewhere");
+		//Module sprayer_somewhere = new Module((float) 0,"sprayer_somewhere");
+		//Module operationnal = new Module((float) 0,"operationnal");
+		//Module board_somewhere = new Module((float) 0,"board_somewhere");
 		
 		//State Goals
-		Module board_sanded = new Module((float) 0,"board_sanded");
-		Module self_painted = new Module((float) 0,"self_painted");
+		//Module board_sanded = new Module((float) 0,"board_sanded");
+		//Module self_painted = new Module((float) 0,"self_painted");
 		
 		//State State
-		Module sprayer_in_hand = new Module((float) 0,"sprayer_in_hand");
-		Module sander_in_hand = new Module((float) 0,"sander_in_hand");
-		Module board_in_hand = new Module((float) 0,"board_in_hand");
-		Module board_in_vise = new Module((float) 0,"board_in_vise");
+		//Module sprayer_in_hand = new Module((float) 0,"sprayer_in_hand");
+		//Module sander_in_hand = new Module((float) 0,"sander_in_hand");
+		//Module board_in_hand = new Module((float) 0,"board_in_hand");
+		//Module board_in_vise = new Module((float) 0,"board_in_vise");
 		
 		Module pick_up_sprayer = new Module((float) 0,"pick_up_sprayer");
-		pick_up_sprayer.addCondition(sprayer_somewhere.toString(), true);
-		pick_up_sprayer.addCondition(hand_is_empty.toString(), true);
-		pick_up_sprayer.addAjoutes(sprayer_in_hand.toString(), false);
-		pick_up_sprayer.addDetruits(sprayer_somewhere.toString(), true);
-		pick_up_sprayer.addDetruits(hand_is_empty.toString(), true);
+		pick_up_sprayer.addCondition("sprayer_somewhere", true);
+		pick_up_sprayer.addCondition("hand_is_empty", true);
+		pick_up_sprayer.addAjoutes("prayer_in_hand", false);
+		pick_up_sprayer.addDetruits("sprayer_somewhere", true);
+		pick_up_sprayer.addDetruits("hand_is_empty", true);
 		
 		Module pick_up_sander = new Module((float) 0,"pick_up_sander");
-		pick_up_sander.addCondition(sander_somewhere.toString(), true);
-		pick_up_sander.addCondition(hand_is_empty.toString(), true);
-		pick_up_sander.addAjoutes(sander_in_hand.toString(), false);
-		pick_up_sander.addDetruits(sander_somewhere.toString(), true);
-		pick_up_sander.addDetruits(hand_is_empty.toString(), true);
+		pick_up_sander.addCondition("sander_somewhere", true);
+		pick_up_sander.addCondition("hand_is_empty", true);
+		pick_up_sander.addAjoutes("sander_in_hand", false);
+		pick_up_sander.addDetruits("sander_somewhere", true);
+		pick_up_sander.addDetruits("hand_is_empty", true);
 		
 		Module pick_up_board = new Module((float) 0,"pick_up_board");
-		pick_up_board.addCondition(board_somewhere.toString(), true);
-		pick_up_board.addCondition(hand_is_empty.toString(), true);
-		pick_up_board.addAjoutes(board_in_hand.toString(), false);
-		pick_up_board.addDetruits(board_somewhere.toString(), true);
-		pick_up_board.addDetruits(hand_is_empty.toString(), true);
+		pick_up_board.addCondition("board_somewhere", true);
+		pick_up_board.addCondition("hand_is_empty", true);
+		pick_up_board.addAjoutes("board_in_hand", false);
+		pick_up_board.addDetruits("board_somewhere", true);
+		pick_up_board.addDetruits("hand_is_empty", true);
 		
 		Module put_down_sprayer = new Module((float) 0,"put_down_sprayer");
-		put_down_sprayer.addCondition(sprayer_in_hand.toString(), false);
-		put_down_sprayer.addAjoutes(sprayer_somewhere.toString(), true);
-		put_down_sprayer.addAjoutes(hand_is_empty.toString(), true);
-		put_down_sprayer.addDetruits(sprayer_in_hand.toString(), false);
+		put_down_sprayer.addCondition("sprayer_in_hand", false);
+		put_down_sprayer.addAjoutes("sprayer_somewhere", true);
+		put_down_sprayer.addAjoutes("hand_is_empty",true);
+		put_down_sprayer.addDetruits("sprayer_in_hand", false);
 		
 		Module put_down_sander = new Module((float) 0,"put_down_sander");
-		put_down_sander.addCondition(sander_in_hand.toString(), false);
-		put_down_sander.addAjoutes(sander_somewhere.toString(), true);
-		put_down_sander.addAjoutes(hand_is_empty.toString(), true);
-		put_down_sander.addDetruits(sander_in_hand.toString(), false);
+		put_down_sander.addCondition("sander_in_hand", false);
+		put_down_sander.addAjoutes("sander_somewhere", true);
+		put_down_sander.addAjoutes("hand_is_empty", true);
+		put_down_sander.addDetruits("sander_in_hand", false);
 		
 		Module put_down_board = new Module((float) 0,"put_down_board");
-		put_down_board.addCondition(board_in_hand.toString(), false);
-		put_down_board.addAjoutes(board_somewhere.toString(), true);
-		put_down_board.addAjoutes(hand_is_empty.toString(), true);
-		put_down_board.addDetruits(board_in_hand.toString(), false);
+		put_down_board.addCondition("board_in_hand", false);
+		put_down_board.addAjoutes("board_somewhere", true);
+		put_down_board.addAjoutes("hand_is_empty", true);
+		put_down_board.addDetruits("board_in_hand", false);
 		
 		//deux fois le module pour check avec les deux mains, dans l'ex de pattie maes
 		Module sand_board_in_hand = new Module((float) 0,"sand_board_in_hand");
-		sand_board_in_hand.addCondition(operationnal.toString(), true);
-		sand_board_in_hand.addCondition(board_in_hand.toString(), false);
-		sand_board_in_hand.addCondition(sander_in_hand.toString(), false);
-		sand_board_in_hand.addAjoutes(board_sanded.toString(), false);
+		sand_board_in_hand.addCondition("operationnal", true);
+		sand_board_in_hand.addCondition("board_in_hand", false);
+		sand_board_in_hand.addCondition("sander_in", false);
+		sand_board_in_hand.addAjoutes("board_sanded", false);
 		
 		Module sand_board_in_vise = new Module((float) 0,"sand_board_in_vise");
-		sand_board_in_vise.addCondition(operationnal.toString(), true);
-		sand_board_in_vise.addCondition(board_in_vise.toString(), false);
-		sand_board_in_vise.addCondition(sander_in_hand.toString(), false);
-		sand_board_in_vise.addAjoutes(board_sanded.toString(), false);
+		sand_board_in_vise.addCondition("operationnal", true);
+		sand_board_in_vise.addCondition("board_in_vise", false);
+		sand_board_in_vise.addCondition("sander_in_hand", false);
+		sand_board_in_vise.addAjoutes("board_sanded", false);
 		
 		Module spray_paint_self = new Module((float) 0,"spray_paint_self");
-		spray_paint_self.addCondition(operationnal.toString(), true);
-		spray_paint_self.addCondition(sprayer_in_hand.toString(), false);
-		spray_paint_self.addAjoutes(self_painted.toString(), false);
-		spray_paint_self.addDetruits(operationnal.toString(), true);
+		spray_paint_self.addCondition("operationnal", true);
+		spray_paint_self.addCondition("sprayer_in_hand", false);
+		spray_paint_self.addAjoutes("self_painted", false);
+		spray_paint_self.addDetruits("operationnal", true);
 		
 		Module place_board_in_vise = new Module((float) 0,"place_board_in_vise");
-		place_board_in_vise.addCondition(board_in_hand.toString(), false);
-		place_board_in_vise.addAjoutes(hand_is_empty.toString(), true);
-		place_board_in_vise.addAjoutes(board_in_vise.toString(), false);
-		place_board_in_vise.addDetruits(board_in_hand.toString(), false);
+		place_board_in_vise.addCondition("board_in_hand", false);
+		place_board_in_vise.addAjoutes("hand_is_empty", true);
+		place_board_in_vise.addAjoutes("board_in_vise", false);
+		place_board_in_vise.addDetruits("board_in_hand", false);
 		
 		Agent a = new Agent();
-		a.addPropositionInitiale(hand_is_empty.toString());
+		a.addPropositionInitiale("hand_is_empty");
+		a.addPropositionInitiale("hand_is_empty");
+
 		//a.addPropositionInitiale(hand_is_empty.toString()); //TODO : Decouvrir pourquoi deux mains font bugguer le programme. (au niveau de la propagation d'energy
-		a.addPropositionInitiale(sander_somewhere.toString());
-		a.addPropositionInitiale(sprayer_somewhere.toString());
-		a.addPropositionInitiale(operationnal.toString());
-		a.addPropositionInitiale(board_somewhere.toString());
+		a.addPropositionInitiale("sander_somewhere");
+		a.addPropositionInitiale("sprayer_somewhere");
+		a.addPropositionInitiale("operationnal");
+		a.addPropositionInitiale("board_somewhere");
 		
-		a.addBut(board_sanded.toString());
-		a.addBut(self_painted.toString());
+		a.addBut("board_sanded");
+		a.addBut("self_painted");
 		
 		e.calculLinkBetweenModules();
 		//D�but de la simulation
 
-		while(Environnement.time < 20) {
+		while(Environnement.time < 3) {
 			System.out.println("TIME : " + Environnement.time);
 			System.out.println(a.printState());
 			//Calcul des activations des modules
