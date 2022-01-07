2505c37381456373b20bd19db40231809493cb34
Maxime <yonnet.maxime@laposte.net>
2022-01-07 18:08:12 +0100

    Debut de travail sur un algorithme semblable à celui du tp

--- SMA/src/vue/Main.java
+++ SMA/src/vue/Main.java
@@ -131,6 +131,123 @@ public class Main {
 		Agent unAgent = new Agent();
 
 		Environnement e = new Environnement(niveauActivationPI, seuilActivationTHETA, energieInjecteeSousButGAMMA, energieInjecteePropositionVraiePHI, energiePriseButProtegeDELTA,unAgent);
+		StatsCreator sc = new StatsCreator(e.getAllModules(),e.getAllTheta());
+		
+		Proposition clef_quelquepart = new Proposition("clef_quelquepart", true);
+		Proposition clef_dans_la_main = new Proposition("clef_dans_la_main", false,2);
+		Proposition etre_proche_de_la_clef = new Proposition("etre_proche_de_la_clef", false);
+		Proposition porte_est_ferme = new Proposition("porte_est_ferme", true);
+		Proposition porte_est_ouverte = new Proposition("porte_est_ouverte", false);
+		Proposition meuble_non_assemble = new Proposition("meuble_non_assemble", true);
+		Proposition meuble_est_assemble = new Proposition("meuble_est_assemble", false);
+		Proposition partie1_du_meuble_quelquepart = new Proposition("partie1_du_meuble_quelquepart", true);
+		Proposition partie1_du_meuble_dans_la_main = new Proposition("partie1_du_meuble_dans_la_main", false);
+		Proposition etre_proche_de_partie1 = new Proposition("etre_proche_de_partie1", false);
+		Proposition etre_proche_de_partie2 = new Proposition("etre_proche_de_partie2", false);
+		Proposition partie2_du_meuble_quelquepart = new Proposition("partie2_du_meuble_quelquepart", true);
+		Proposition partie2_du_meuble_dans_la_main = new Proposition("partie2_du_meuble_dans_la_main", false);
+		Proposition A_Cherche_Clef = new Proposition("A_Cherche_Clef", false);
+		Proposition A_Cherche_partie1 = new Proposition("A_Cherche_partie1", false);
+		Proposition A_Cherche_partie2 = new Proposition("A_Cherche_partie2", false);
+		
+		Module ouvrir_porte = new Module(0,"pick_up_sprayer");
+		ouvrir_porte.addCondition(clef_dans_la_main);
+		ouvrir_porte.addCondition(porte_est_ferme);
+		ouvrir_porte.addAjoutes(porte_est_ouverte);
+		ouvrir_porte.addDetruits(porte_est_ferme);
+		
+		Module chercher_clef = new Module(0,"pick_up_sprayer");
+		chercher_clef.addCondition(sprayer_somewhere);
+		chercher_clef.addCondition(hand_is_empty);
+		chercher_clef.addAjoutes(sprayer_in_hand);
+		chercher_clef.addDetruits(sprayer_somewhere);
+		chercher_clef.addDetruits(hand_is_empty);
+		
+		Module chercher_partie1 = new Module(0,"pick_up_sprayer");
+		chercher_partie1.addCondition(sprayer_somewhere);
+		chercher_partie1.addCondition(hand_is_empty);
+		chercher_partie1.addAjoutes(sprayer_in_hand);
+		chercher_partie1.addDetruits(sprayer_somewhere);
+		chercher_partie1.addDetruits(hand_is_empty);
+		
+		Module chercher_partie2 = new Module(0,"pick_up_sprayer");
+		chercher_partie2.addCondition(sprayer_somewhere);
+		chercher_partie2.addCondition(hand_is_empty);
+		chercher_partie2.addAjoutes(sprayer_in_hand);
+		chercher_partie2.addDetruits(sprayer_somewhere);
+		chercher_clef.addDetruits(hand_is_empty);
+		
+		Module prendre_clef = new Module(0,"pick_up_sprayer");
+		prendre_clef.addCondition(sprayer_somewhere);
+		prendre_clef.addCondition(hand_is_empty);
+		prendre_clef.addAjoutes(sprayer_in_hand);
+		prendre_clef.addDetruits(sprayer_somewhere);
+		prendre_clef.addDetruits(hand_is_empty);
+		
+		Module prendre_partie1 = new Module(0,"pick_up_sprayer");
+		prendre_clef.addCondition(sprayer_somewhere);
+		prendre_clef.addCondition(hand_is_empty);
+		prendre_clef.addAjoutes(sprayer_in_hand);
+		prendre_clef.addDetruits(sprayer_somewhere);
+		prendre_clef.addDetruits(hand_is_empty);
+		
+		Module prendre_partie2 = new Module(0,"pick_up_sprayer");
+		prendre_clef.addCondition(sprayer_somewhere);
+		prendre_clef.addCondition(hand_is_empty);
+		prendre_clef.addAjoutes(sprayer_in_hand);
+		prendre_clef.addDetruits(sprayer_somewhere);
+		prendre_clef.addDetruits(hand_is_empty);
+		
+		Module aller_vers_clef = new Module(0,"pick_up_sprayer");
+		aller_vers_clef.addCondition(sprayer_somewhere);
+		aller_vers_clef.addCondition(hand_is_empty);
+		aller_vers_clef.addAjoutes(sprayer_in_hand);
+		aller_vers_clef.addDetruits(sprayer_somewhere);
+		aller_vers_clef.addDetruits(hand_is_empty);
+		
+		Module aller_vers_partie1 = new Module(0,"pick_up_sprayer");
+		aller_vers_partie1.addCondition(sprayer_somewhere);
+		aller_vers_partie1.addCondition(hand_is_empty);
+		aller_vers_partie1.addAjoutes(sprayer_in_hand);
+		aller_vers_partie1.addDetruits(sprayer_somewhere);
+		aller_vers_partie1.addDetruits(hand_is_empty);
+		
+		Module aller_vers_partie2 = new Module(0,"pick_up_sprayer");
+		aller_vers_partie2.addCondition(sprayer_somewhere);
+		aller_vers_partie2.addCondition(hand_is_empty);
+		aller_vers_partie2.addAjoutes(sprayer_in_hand);
+		aller_vers_partie2.addDetruits(sprayer_somewhere);
+		aller_vers_partie2.addDetruits(hand_is_empty);
+		
+		Module assembler_meuble = new Module(0,"pick_up_sprayer");
+		assembler_meuble.addCondition(sprayer_somewhere);
+		assembler_meuble.addCondition(hand_is_empty);
+		assembler_meuble.addAjoutes(sprayer_in_hand);
+		assembler_meuble.addDetruits(sprayer_somewhere);
+		assembler_meuble.addDetruits(hand_is_empty);
+
+
+		
+
+		while(!unAgent.isDone()) {
+			System.out.println("\n\nTIME : " + e.time);
+			//unAgent.printState();
+			//Calcul des activations des modules
+			e.executable();
+			// Diffusion d'énergie d'activation
+
+			e.updateEnergy(false);
+			
+			//e.afficherEtatActivation();
+
+			//Si executable, Execution d'un module
+			e.execute();
+			e.updateTheta();
+			e.time++;
+		}
+		System.out.println("\nLes missions ont été terminés en " + e.time );
+		sc.exportToExcel("test1.txt");
+		sc.exportToPng("Seuils4");
 		
 		
 		
